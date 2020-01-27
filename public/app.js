1f95b673e3efd64fb9f6f069ec4d44469bc76011
Blobcode <lhelme123@gmail.com>
2020-01-27 07:16:33 -0500

    Added login / signup js.

--- public/app.js
+++ public/app.js
@@ -1,6 +1,9 @@
+const suemail = document.getElementById("suemail")
+const supassword = document.getElementById("supassword")
+
 document.addEventListener("DOMContentLoaded", event => {
-    const db = firebase.firestore();
-    const app = firebase.app();
+    db = firebase.firestore();
+    app = firebase.app();
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
             window.location.href = "/home"
@@ -12,15 +15,29 @@ document.addEventListener("DOMContentLoaded", event => {
 
 
 function signIn() {
-    const provider = new.signInWithEmailAndPassword(email, password);
     //Sign in with google.
-    firebase.auth().signInWithRedirect(provider)
-        .then(result => {
-            window.location.reload();
-        })
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.error(errorCode, errorMessage)
+        // ...
+    })
 }
 
 function signUp() {
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-    window.location.href = "/home"
+    console.log(document.getElementById("supassword"))
+    console.log(suemail, supassword)
+    firebase.auth().createUserWithEmailAndPassword(suemail.value, supassword.value)
+        .catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            if (errorCode == 'auth/weak-password') {
+                alert('The password is too weak.');
+            } else {
+                alert(errorMessage);
+            }
+            console.log(error);
+        });
 }
