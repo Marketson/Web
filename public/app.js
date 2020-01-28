2b3e9de8a3eb5054e0cc8a2d10e15d1d976fbea0
Blobcode <lhelme123@gmail.com>
2020-01-28 07:14:24 -0500

    Added Signup

--- public/app.js
+++ public/app.js
@@ -1,28 +1,40 @@
+const suusername = document.getElementById("suusername")
 const suemail = document.getElementById("suemail")
 const supassword = document.getElementById("supassword")
+const siemail = document.getElementById("siemail")
+const sipassword = document.getElementById("sipassword")
 
 document.addEventListener("DOMContentLoaded", event => {
     db = firebase.firestore();
     app = firebase.app();
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            window.location.href = "/home"
-        } else {
-            // No user is signed in.
-        };
-    });
+})
+
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        window.location.href = "/home"
+    } else {
+        // No user is signed in.
+    };
 });
 
 
 function signIn() {
     //Sign in with google.
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.error(errorCode, errorMessage)
-        // ...
-    })
+    console.log(document.getElementById("supassword"))
+    console.log(suemail, supassword)
+    firebase.auth().signInWithEmailAndPassword(siemail.value, sipassword.value)
+        .catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            if (errorCode == 'auth/weak-password') {
+                alert('The password is too weak.');
+            } else {
+                alert(errorMessage);
+            }
+            console.log(error);
+        });
 }
 
 function signUp() {
@@ -40,4 +52,4 @@ function signUp() {
             }
             console.log(error);
         });
-}
+};
---
+++ public/home/app.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", event => {
+    //db = firebase.firestore();
+    app = firebase.app();
+})
+
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        guser = user
+        console.log(user.displayName)
+        if (user.displayName === null) {
+            //get user to fill out form
+            $('#staticBackdrop').modal()
+        }
+    } else {
+        window.location.href = "/"
+    };
+});
+
+
+function setName() {
+    user.updateProfile({
+        displayName: document.getElementById("suusername").value,
+    })
+}
