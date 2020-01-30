b93fbba785eb6415709b342545a3303964346ed1
Blobcode <lhelme123@gmail.com>
2020-01-30 07:06:50 -0500

    view listings

--- public/home/app.js
+++ public/home/app.js
@@ -1,12 +1,20 @@
 document.addEventListener("DOMContentLoaded", event => {
     db = firebase.firestore();
     app = firebase.app();
+    const listeditems = db.collection("listing")//.where('creator', '==', curuser.uid)
+    listeditems.get()
+        .then(listing => {
+            listing.forEach(doc => {
+                var data = doc.data()
+                $(`<a onclick="viewListingOwn(${JSON.stringify(data.title).replace(/"/g, "&quot;")}, ${JSON.stringify(data.description).replace(/"/g, "&quot;")})" class="list-group-item list-group-item-action" >${data.title}</a>`).appendTo('#my-listed-items')
+            })
+        })
 })
 
 
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-        guser = user
+        curuser = user
         console.log(user.displayName)
         if (user.displayName === null) {
             //get user to fill out form
@@ -35,4 +43,35 @@ function signOut() {
         alert(error)
     });
 
+}
+
+function addListing() {
+
+}
+
+function deleteListing(listing) {
+
+}
+
+function viewListing() {
+
+}
+
+function viewListingOwn(title = "ERR", description = "ERROR") {
+    bootbox.dialog({
+        title: title,
+        message: $(`<p>${description}</p>`),
+        size: 'large',
+        buttons: {
+            cancel: {
+                label: "Close",
+                className: 'btn-secondary',
+            },
+            ok: {
+                label: "Delete listing.",
+                className: 'btn-danger',
+                callback: deleteListing()
+            }
+        }
+    });
 }
